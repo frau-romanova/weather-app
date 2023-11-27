6d92949fde2b0f726d327fbb2fd216d8faf4978d
frau-romanova <valeria.ignite@gmail.com>
2023-11-27 15:20:06 +0100

    added API

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ function refreshWeather(response) {
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+
+getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -41,7 +43,7 @@ function formatDate(date) {
 
 function searchCity(city) {
   let apiKey = "b2a5adcct04b33178913oc335f405433";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(refreshWeather);
 }
 
@@ -50,8 +52,17 @@ function handleSearchSubmit(event) {
   let searchInput = document.querySelector("#search-form-input");
  searchCity(searchInput.value);
 }
+function getForecast(city) {
+  let apiKey = "530a8216c4tdaaa3fo301733fcbd7d33";
+  let apiUrl =
+    `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+axios(apiUrl).then(displayForecast))
+console.log(apiUrl);
+}
 
-function displayForecast () {
+function displayForecast (response) {
+  console.log (response.data);
+
 let days = ["Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 let forecastHTML = "";
 
@@ -77,4 +88,4 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Gevgelija");
-displayForecast ();
+
